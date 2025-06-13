f9ee48685b97f5301be913750b1f683585eb00d8
lucasdemouramacedo <lucasdemouramacedo@gmail.com>
2025-06-13 09:45:47 -0300

    feat: add create user

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\UserService;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function __construct(
+        protected UserService $userService
+    ) {}
+
+    /**
+     * Create a new User
+     */
+    public function create(Request $request)
+    {
+            $user = $this->userService->createUser($request->all());
+            return response()->json($user, 201);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Ramsey\Uuid\Nonstandard\Uuid;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+
+    public $keyType = 'string';
+
+    public $incrementing = false;
 
     /**
      * The attributes that are mass assignable.
@@ -31,6 +36,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'email',
+        'updated_at',
+        'created_at'
     ];
 
     /**
@@ -43,5 +51,13 @@ class User extends Authenticatable
         return [
             'password' => 'hashed',
         ];
+    }
+
+    public static function boot() {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->id = (string) Uuid::uuid4();
+        });
     }
 }
