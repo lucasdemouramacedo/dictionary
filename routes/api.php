524b8604efc98fe09f69b8bc5806ab12921b24bd
lucasdemouramacedo <lucasdemouramacedo@gmail.com>
2025-06-15 12:55:44 -0300

    feat: add default controller

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DefaultController;
 use App\Http\Controllers\FavoriteWordsController;
 use App\Http\Controllers\HistoryController;
 use App\Http\Controllers\SearchController;
@@ -8,6 +9,8 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\WordController;
 use App\Http\Middleware\AuthMiddleware;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/', DefaultController::class);
 
 Route::prefix('auth')->group(function () {
     Route::post('/signup', [UserController::class, 'create']);
